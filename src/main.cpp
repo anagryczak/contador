f0e18905b569138bfc7663ae1b4f619dd7e4c404
anagryczak <anagevert@alunos.utfpr.edu.br>
2024-02-14 18:11:38 -0300

    oiiie

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,41 @@
 #include <Arduino.h>
 
-const int botao = 1;
+const int botao = 3;
+const int led = 8;
+const int led2 = 9;
 
-int contador = 0;
-bool contAnterior = LOW;
-bool contAtual = LOW;
+int contador = 0; 
+
+void interromper () {
+
+  digitalWrite (led,LOW);
+  digitalWrite(led2, HIGH);
+  delay(2000);
+  digitalWrite(led2, LOW);
+  delay(500);
+  contador = contador + 1;
+  Serial.print("apertos: ");
+  Serial.println(contador);
+
+}
 
 void setup() {
 
 pinMode(botao,INPUT);
+pinMode(led, OUTPUT);
+pinMode(led2, OUTPUT);
+
 Serial.begin(9600);
+
+attachInterrupt(digitalPinToInterrupt(botao), interromper, CHANGE);
 
 }
 
 void loop() {
 
-  contAtual = digitalRead(botao);
-
-  if (contAtual == HIGH && contAnterior == LOW){
-
-    contador = contador + 1;
-    Serial.println("apertos:");
-    Serial.println (contador);
-  }
-
-     contAnterior = contAtual;
-
+  digitalWrite (led, HIGH);
+  delay(1000);
+  digitalWrite (led, LOW);
+  delay(1000);
+  
 }
